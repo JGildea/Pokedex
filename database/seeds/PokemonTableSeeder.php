a9ba75015e4d8f7ec39b6ab7a6dbd58971ff866c
jgildea <josephlgildea@gmail.com>
2019-06-21 01:34:25 -0500

    cleaned it up a bit

--- database/seeds/PokemonTableSeeder.php
+++ database/seeds/PokemonTableSeeder.php
@@ -16,36 +16,28 @@ class PokemonTableSeeder extends Seeder
         $data = json_decode($json);
         foreach ($data as $obj) {
             $type2 = $ability2 = $ability3 = $egg_group2 = null;
- //           $array = $obj->types;
-//            dd(count($obj->types));
+
             for( $i = 0; $i < count($obj->types); $i++){
-                 //if($obj->types[$i] != null){
+
                     ${'type'.($i+1)}= $obj->types[$i];
-                 //}
+
             }
             for( $i = 0; $i < count($obj->abilities); $i++){
-                //if($obj->abilities[$i] != null){
+
                     ${'ability'.($i+1)} = $obj->abilities[$i];
-                //}
+
              }
         
-//            dd($pokeArray);
+        
             for( $i = 0; $i < count($obj->egg_groups); $i++){
-               //if($obj->egg_group[$i] != null){
+
                     ${'egg_group'.($i+1)} = $obj->egg_groups[$i];
-               //}
+
             }
-            //dd($obj->stats->hp);
-            //$stats = json_decode($obj->stats);
-            //dd($obj->stats->special_attack);
+
             pokemon::create(array(
                 'id' => $obj->id,
                 'name' => $obj->name,
-
-                // for( $i = 0; $i < count(array($obj->types)); $i++){
-                //     'type'.$i => array($obj->types)->$i;
-                // }
-//                'types' => $obj->types,
                 'type1' => $type1,
                 'type2' => $type2,
                 'height' => $obj->height,
@@ -53,16 +45,6 @@ class PokemonTableSeeder extends Seeder
                 'ability1' => $ability1,
                 'ability2' => $ability2,
                 'ability3' => $ability3,
-//                'abilities' => $obj->abilities,
-                // for( $i = 0; $i < count(array($obj->abilities)); $i++){
-                //     'ability'.$i => array($obj->abilities)->$i;
-
-                // }
-//                'egg_groups' => $obj->egg_groups,
-                // for( $i = 0; $i < count(array($obj->egg_groups)); $i++){
-                //     'egg_group'.$i => array($obj->egg_groups)->$i;
-                // }
-//                'stats' => $obj->stats,
                 'egg_group1' => $egg_group1,
                 'egg_group2' => $egg_group2,
 
--- routes/api.php
+++ routes/api.php
@@ -18,22 +18,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
+
 Route::group(['middleware' => 'auth:api'], function(){
-Route::post('details', 'API\UserController@details');
-Route::get('pokemon/my', function(){
-    $pokemon = Auth::user()->pokemon()->orderBy('id')->get();
-    return $pokemon;
-});
-Route::get('pokemon/{id}/catch', function($pid){
-         $user = Auth::user(); 
-         $user->pokemon()->sync([($pid)], false); 
+    Route::post('details', 'API\UserController@details');
+    // allows a user to see the pokemon that they have
+    Route::get('pokemon/my', function(){
+        $pokemon = Auth::user()->pokemon()->orderBy('id')->get();
+        return $pokemon;
+    });
+    //takes a pokemon id number and marks it ass caught by the logged in user
+    Route::get('pokemon/{id}/catch', function($pid){
+        $user = Auth::user(); 
+        $user->pokemon()->sync([($pid)], false); 
 
+    });
 });
-});
-
+//return a specific pokemon
 Route::get('pokemon/{id}', function ($id) {
     return pokemon::find($id);
 });
+//return a simple paginated list
 Route::get('pokemon', function() {
     return pokemon::simplepaginate(10);
 });
